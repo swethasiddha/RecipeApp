7548f7ed31db8110356fb9426b3a935b98f75a78
swethasiddha <siddhaswetha710@gmail.com>
2024-11-18 14:16:22 +0530

    Add files via upload

---
+++ src/utils/userData.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+    userName: "swetha",
+    password: "swetha123",
+  },
+  {
+    userName: "siddha",
+    password: "siddha123",
+  },
+  {
+    userName: "reddy",
+    password: "reddy123",
+  },
+];
+
+export default users;
